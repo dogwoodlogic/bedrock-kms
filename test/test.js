662c2e1dd5d65a4b5c57617ef51fb01d8d170b81
Matthew Collier <mcollier@digitalbazaar.com>
2020-09-25 16:03:52 -0400

    Update copyright notices.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019-2020 Digital Bazaar, Inc. All rights reserved.
  */
+'use strict';
+
 const bedrock = require('bedrock');
 require('bedrock-kms');
 require('bedrock-ssm-mongodb');
