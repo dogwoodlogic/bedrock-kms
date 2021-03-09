8cdd40d16c0b2a807cbf9bb4a00001ddc14cc982
Matthew Collier <mcollier@digitalbazaar.com>
2021-03-09 15:02:45 -0500

    Remove allowedHost config.

--- test/test.config.js
+++ test/test.config.js
@@ -13,5 +13,3 @@ config.mongodb.name = 'bedrock_kms_test';
 config.mongodb.dropCollections = {};
 config.mongodb.dropCollections.onInit = true;
 config.mongodb.dropCollections.collections = [];
-
-config.kms.allowedHost = 'localhost';
